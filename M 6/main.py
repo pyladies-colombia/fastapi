72c34efa2477d08219eefa8a6b5faa8e872f8e5a
Alejandra <90076947+alejsdev@users.noreply.github.com>
2024-06-05 23:17:16 -0500

    Update M 6/main.py
    Co-authored-by: Sebastián Ramírez <tiangolo@gmail.com>

--- M 6/main.py
+++ M 6/main.py
@@ -24,6 +24,7 @@ SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
 
+
 @app.post("/reservations/")
 def create_reservation(reservation: ReservationBase):
     with Session(engine) as session:
